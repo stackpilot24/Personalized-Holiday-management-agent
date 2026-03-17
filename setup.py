d2313aa140292c54799873089b13df4cfe51dd58
stackpilot24 <samyakjain1524@gmail.com>
2026-03-17 22:39:50 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='holiday_management',
+    version='0.1.0',
+    author='Samyak Jain',
+    author_email='jainsamyak905@gmail.com',
+    packages=find_packages(),
+)
